a37c658953cf35e2d442903f337c24e6be594fa8
Leo470-design <69416568+Leo470-design@users.noreply.github.com>
2025-02-14 19:43:01 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReferEarn from "./ReferEarn";
+import { CssBaseline } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <ReferEarn />
+    </>
+  );
+}
+
+export default App;
